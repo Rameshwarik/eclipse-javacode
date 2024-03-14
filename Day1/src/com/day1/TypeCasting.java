a9232b3a30309de3421dddf29615b6f5d47ed608
rameshwarikadrame123 <rameshwarikadrame123@gmail.com>
2024-03-15 00:25:03 +0530

    updated

---
+++ Day1/src/com/day1/TypeCasting.java
@@ -0,0 +1,53 @@
+package com.day1;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+		
+		//widening/impicit type casting
+		
+		byte b=10;
+		int i=b;
+		System.out.println(i);
+		
+		float f = 22.14f;
+		
+		double d= f;
+		System.out.println(d);
+		
+		char ch = 'A';
+		int i3 = ch ;
+		System.out.println(13);
+		
+		char var1 = '\u00A7';
+		int i4 = var1;
+		System.out.println(i4);
+		
+		System.out.println("-------------------------------------------------------------");
+		
+		//narrowing // explicit type casting 
+		double f1 = 10.52f;
+		long l = (long) f1;
+		System.out.println(l);
+		
+		//long ll = 9233720368547758061;
+		//int i2 = (int) ll;
+		//System.out.println(i2);
+		
+		float f2 = 34.56f;
+		int i1 = (int)f2;
+		System.out.println(i1);
+		
+		byte b1 = 90;
+		char ch1 = (char)b1;
+		System.out.println(ch1);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
